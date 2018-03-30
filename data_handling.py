d1d3733c000507d48274d13819a39ef021e7f4f6
Forrest Hooton <fhooton@crimson.ua.edu>
2018-03-30 14:57:28 -0400

    update

--- data_handling.py
+++ data_handling.py
@@ -2,76 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import pandas as pd
-
 import numpy as np
-
 from time import time
-
 from skimage import data, transform
-
 import tensorflow as tf
-
 import matplotlib.pyplot as plt
-
 import config
-
 import pickle
-
 import fnmatch
-
 import os
-       
-
 
 
 def image_data_subset(filetype, perc = .1):
     
     filename = "/Users/forresthooton/Documents/Masters Classes/Supervised Machine Learning/Class Project/" + filetype + "_image_data.csv"
-    
     data = pd.read_csv(filename)
     
     # Sets the number of images we want to train based off a percent of the total number
-    
     numImages = round(data.shape[0] * perc)
     
     # Gets random DataFrame subset where the number of rows is numImages
-    
     data = data.loc[np.random.choice(data.index,numImages,False)].reset_index(drop=True).copy()
     
     return data
     
 
-
-
 def attach_annotations(imgData, annData):
     
     start = time()
-    
     condencedData = pd.DataFrame()
     
     # Cycles through each image in the image data table
-    
-    
-    
     for img in imgData['image_id']:
         
         # Returns the subset of annotations with a particular image_id
-        
         annSub = annData[annData['image_id'] == img]
-        
         annDict = {}
         
         # Goes through each row of annotation subset for a specific image id and makes a single dictionary
         # where the key is the caption_id and the value is the caption
-        
-        
-        
         for idx, row in annSub.iterrows():
-            
             annDict[row['caption_id']] = row['caption']
         
         # Makes a pd.Series to updata a new df with the image_id and associated annotations
-        
         entry = pd.Series()
         
         entry['image_id'] = img
@@ -79,15 +52,11 @@ def attach_annotations(imgData, annData):
         entry['annotations'] = annDict
         
         # Adds column into df
-        
         condencedData = pd.concat([condencedData, entry], 1)
-    
-    
     
     condencedData = condencedData.T
     
     # Merges the new df of image_id's and annotations into image data table
-    
     imgData = imgData.merge(condencedData, on = 'image_id', how = 'left')
     
     print("DONE IN", round(time() - start), "SEC")
@@ -95,47 +64,27 @@ def attach_annotations(imgData, annData):
     return imgData
 
 
-
-
 # Attaches annotations as dict to image data
-    
 def reorganize_data_tables():
     
     # Returns DataFrame of randomly chosen rows
-    
     trainSubset = image_data_subset("train")
-    
     valSubset = image_data_subset("val")
-    
-    
-    
+
     # Rename id for clarity throughout the project
-    
     trainSubset = trainSubset.rename(columns = {'id':'image_id'})
-    
     valSubset = valSubset.rename(columns = {'id':'image_id'})
-    
+
     trainAnns = pd.read_csv("/Users/forresthooton/Documents/Masters Classes/Supervised Machine Learning/Class Project/train_annotations.csv")
-    
     valAnns = pd.read_csv("/Users/forresthooton/Documents/Masters Classes/Supervised Machine Learning/Class Project/val_annotations.csv")
     
-    
-    
     # Rename id for clarity throughout the project
-    
     trainAnns = trainAnns.rename(columns = {'id':'caption_id'})
-    
     valAnns = valAnns.rename(columns = {'id':'caption_id'})
     
-    
-    
     # Assign the subset of annotations in which the image_id is in the image data subset to a new var
-    
     trainAnnSubset = trainAnns[trainAnns['image_id'].isin(trainSubset['image_id'])]
-    
     valAnnSubset = valAnns[valAnns['image_id'].isin(valSubset['image_id'])]
-    
-    
     
     # attach_annotations() returns a DataFrame that is the image data subset with an annotations column attached
     # The values in the annotations column are dictionaries, where the key is the 'caption_id'
@@ -143,21 +92,16 @@ def reorganize_data_tables():
     # annotations for an image
     
     trainSubset = attach_annotations(trainSubset.copy(), trainAnnSubset.copy())
-    
     valSubset = attach_annotations(valSubset.copy(), valAnnSubset.copy())
     
     trainSubset.to_pickle("train_data.pkl")
-    
     valSubset.to_pickle("val_data.pkl")
-
 
 
 
 def resize_images():
     
     start = time()
-    
-    
     
     trainData = pd.read_pickle("train_data.pkl")
     
@@ -174,7 +118,6 @@ def resize_images():
     for f in image_filenames:
         
         filepath = os.path.join(data_directory, f)
-        
         outpath = os.path.join(output_directory, f)
         
         image = data.imread(filepath)
@@ -191,30 +134,19 @@ def resize_images():
         
         plt.imshow(data.imread(outpath))
     
-    
-    
     print("Standardization DONE IN", round(time() - start), "SEC")
-
-
 
 
 def tokenize_captions():
     
     data = pd.read_pickle("train_data.pkl")
-    
     captions = []
     
-    
-    
     # Append all captions to one list
-    
     for idx, row in data.iterrows():
-        
         for key, value in row['annotations'].items():
             
             captions.append(value)
-    
-    
     
     joined_cap = " ".join(captions) # Join all captions into one string
     
@@ -226,8 +158,6 @@ def tokenize_captions():
     
     with open('tokens.pkl', 'wb') as f:
         pickle.dump(unique_tokens, f)
-    
-    
     
     
 def main():
--- model.py
+++ model.py
@@ -2,33 +2,23 @@
 # -*- coding: utf-8 -*-
 
 import tensorflow as tf
-
 import numpy as np
-
 import matplotlib as plt
-
 import config
-
 from time import time
-
-
 
 
 def example_weight_initialization():
     
     n_features = X_values.shape[1]
-    
     n_classes = len(set(y_flat))
-    
     weights_shape = (n_features, n_classes)
     
     W = tf.Variable(dtype=tf.float32, initial_value=tf.random_normal(weights_shape))  # Weights of the model
 
 
 
-
 # Helper function to simply run a variable
-
 def run_variable(variable):
     
     tf.initialize_all_variables()
@@ -39,100 +29,116 @@ def run_variable(variable):
 
 
 
-
-
 def cnn():
     
     # Include these with statements to understand tensorboard better
-    
     with tf.name_scope("CNN"):
-        
+
         with tf.name_scope("Input_layer"):
             
             # Below is an example from other code
-           
-            x = tf.placeholder(dtype = tf.float32, shape = [None, config.IMG_HEIGHT, config.IMG_WIDTH], name = "image_input")
+            image = tf.placeholder(dtype = tf.float32, shape = [None, config.IMG_HEIGHT, config.IMG_WIDTH], name = "image_input")
             
-            
+            # Flattens image to transform into input layer
+            input_layer = tf.contrib.layers.flatten(image)
             
         with tf.name_scope("Hidden_layer"):
             
             # Examples
-            
             conv1 = tf.layers.conv2d(
-                    
                     inputs=input_layer, 
-                    
                     filters=32,
-                    
                     kernel_size=[5, 5],
-                    
                     padding="same", 
-                    
                     activation=tf.nn.relu,
-                    
                     name = "conv1")
             
             pool1 = tf.layers.max_pooling2d(
-                    
                     inputs=conv1,
-                    
                     pool_size=[2,2],
-                    
                     strides=2,
-                    
                     name = "pool1")
-            
-            
             
         with tf.name_scope("Output_layer"):
             
             # Or whatever kind of layer we want to output
-            
             output_layer = tf.layers.conv2d(
-                    
                     inputs= pool1, # or whatever last layer before output
-                    
                     filters=32,
-                    
                     kernel_size=[5, 5],
-                    
                     padding="same", 
-                    
                     activation=tf.nn.relu,
-                    
                     name = "output_layer")
-        
-        
+
         
         return output_layer
 
-
-
-
-def model(image):
+# Goal of model() is to initialize all the variables we will need in training, thus initializing the structure
+# Then, wherever the variables go, goal is to minimize loss over some sort of optimization, like the Adam optimizer
+def model():
     
-    cnn_output = tf.placeholder(cnn(image), name = "CNN_output")
-    
-    
+    cnn_output = tf.get_variable(cnn(), name = "CNN_output")
     
     with tf.name_scope("lstm"):
-    
+            
+        # Initialize lstm cell
+        lstm = tf.contrib.rnn.BasitcLSTMCell()        
+        
+        with tf.name_scope("word_embedding"):
+            
+            embedding_matrix = tf.get_variable(
+                    shape = [config.NUM_TOKENS, config.DIM_EMBEDDING],
+                    name = 'embedding_weights')
+            
+            embedded_word = tf.nn.embedding_loopup(embedding_matrix, last_word)
+        
         with tf.name_scope("Inputs"):
             
-            current_input = tf.concat([cnn_output, "placeholder for word"], 1)
+            # Initialize lstm cell
+            lstm = tf.contrib.rnn.BasitcLSTMCell()
             
+            # Initialize layers
+            hidden_state = tf.zeros([batch_size, lstm.state_size])
+            current_state = tf.zeros([batch_size, lstm.state_size])
+            
+            state = hidden_state, current_state
+            
+            # I think the 1 here keeps the embedded word from simply being appended as another facet of convolution
+            current_input = tf.concat([cnn_output, embedded_word], 1)
     
-    
+            
     output, state = lstm(current_input, state)
 
-
-
-
+# This is what it we'll use to actually train the model, wether in a different train function or train file
 def main():
-    CNN()
-        
     
+    if not os.path.exists(config.SUMMARY_DIRECTORY):
+            os.mkdir(config.SUMMARY_DIRECTORY)
+    
+    
+    with tf.Session() as sess:
+        
+        summ_writer = tf.summary.FileWriter(config.SUMMARY_DIRECTORY,
+                                            sess.graph)
+        
+        # Space to build model and other variables like optimizer
+        
+        
+        # Need to initialize all varialbes after they are declared
+        sess.run(tf.global_variables_initializer())
+        
+        for _ in epoch:
+            for _ in batch:
+                
+                
+                
+                feed_dict = 
+    
+    
+    
+    
+    
+        summ_writer.close()
     
 
 if __name__ == "__main__":
