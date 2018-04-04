a4ea62e0e1ea60da3219155af1a9d74cdb1afa5a
Ubuntu <ubuntu@ip-172-31-0-166.us-east-2.compute.internal>
2018-04-04 17:21:43 +0000

    Update

--- helperFunctions.py
+++ helperFunctions.py
@@ -7,8 +7,11 @@ Created on Tue Apr  3 18:36:03 2018
 
 import pandas as pd
 import numpy as np
+from skimage import data
+import os
+import config
 
-def getImageBatchFromPickle(batchSize,pkl):
+def getImageBatchFromPickle(pkl, data_directory):
     '''
     Gets the image batch and the corresponding captions. Takes a pickle file
     and then randomly select batchSize indices. Then we locate the those
@@ -16,11 +19,23 @@ def getImageBatchFromPickle(batchSize,pkl):
     and the idx_captions.
     '''
     df = pd.read_pickle(pkl)
-    imageBatchIndex = np.random.choice(df.index,size=batchSize)
-    imageBatch=df.iloc[imageBatchIndex][['file_name','idx_caption_matrix']]
+    imageBatchIndex = np.random.choice(df.index,size=config.BATCH_SIZE)
+    imageBatch=df.iloc[imageBatchIndex][['file_name','mapped_captions']]
     
+    # Just gets a couple images and captions for testing right now
+    image_filenames = list(imageBatch['file_name'])
+    print(image_filenames)
     
-    return imageBatch
+    images = []
+    captions = []
+    
+    for f in image_filenames:
+        filepath = os.path.join(data_directory, f)
+        images.append(data.imread(filepath))
+        cap_row = imageBatch[imageBatch['file_name'] == f]
+        captions.append(list(cap_row['mapped_captions'].item()))
+    
+    return images, captions
 
     
 
